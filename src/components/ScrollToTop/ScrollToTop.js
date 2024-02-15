c44da95fd574d09b3f4eeb8cf6290328f97bbe3d
Jesvin Jose <jesvinjose49@gmail.com>
2024-02-15 12:34:35 +0530

    Yarn related issues were solved

--- src/components/ScrollToTop/ScrollToTop.js
+++ src/components/ScrollToTop/ScrollToTop.js
@@ -1,3 +1,29 @@
+// import React, { useEffect, useState } from 'react'
+// import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward'
+// import './ScrollToTop.css'
+
+// const ScrollToTop = () => {
+//   const [isVisible, setIsVisible] = useState(false)
+
+//   useEffect(() => {
+//     const toggleVisibility = () =>
+//       window.pageYOffset > 500 ? setIsVisible(true) : setIsVisible(false)
+
+//     window.addEventListener('scroll', toggleVisibility)
+//     return () => window.removeEventListener('scroll', toggleVisibility)
+//   }, [])
+
+//   return isVisible ? (
+//     <div className='scroll-top'>
+//       <a href='#top'>
+//         <ArrowUpwardIcon fontSize='large' />
+//       </a>
+//     </div>
+//   ) : null
+// }
+
+// export default ScrollToTop
+
 import React, { useEffect, useState } from 'react'
 import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward'
 import './ScrollToTop.css'
@@ -15,7 +41,7 @@ const ScrollToTop = () => {
 
   return isVisible ? (
     <div className='scroll-top'>
-      <a href='#top'>
+      <a href='#top' aria-label='Scroll to top'>
         <ArrowUpwardIcon fontSize='large' />
       </a>
     </div>
